d2ebd0bee74e2ecbccf0686e68a54704fee24d0d
llw <llw18729357726@163.com>
2024-03-25 22:34:40 +0800

    初始结构

--- gulimall-product/src/main/java/com/aitiguigu/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/aitiguigu/gulimall/product/GulimallProductApplication.java
@@ -1,8 +1,10 @@
 package com.aitiguigu.gulimall.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.aitiguigu.gulimall.product.dao")
 @SpringBootApplication
 public class GulimallProductApplication {
 
--- gulimall-product/src/test/java/com/aitiguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/aitiguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -1,13 +1,25 @@
 package com.aitiguigu.gulimall.product;
 
+import com.aitiguigu.gulimall.product.entity.BrandEntity;
+import com.aitiguigu.gulimall.product.service.BrandService;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class GulimallProductApplicationTests {
 
+    @Autowired
+    BrandService brandService;
+
     @Test
     void contextLoads() {
+        System.out.println("查询成功");
+        List<BrandEntity> list = brandService.list();
+        System.out.println(list);
     }
 
 }
