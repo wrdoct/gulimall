ca12f1519fca89ba3724e7875c718043d132fe60
llw <llw18729357726@163.com>
2024-03-27 11:27:40 +0800

    代码生成器生成初步代码

--- gulimall-order/src/main/java/com/aitiguigu/gulimall/order/GulimallOrderApplication.java
+++ gulimall-order/src/main/java/com/aitiguigu/gulimall/order/GulimallOrderApplication.java
@@ -1,8 +1,12 @@
 package com.aitiguigu.gulimall.order;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
+@MapperScan("com.aitiguigu.gulimall.order.dao")
 @SpringBootApplication
 public class GulimallOrderApplication {
 
